f1c60372fe3579bad1fda28733a11017eb4b859b
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-04-10 10:33:19 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,38 @@
 input.onSound(DetectedSound.Loud, function () {
+    basic.showLeds(`
+        . # # . .
+        # . . # .
+        # . # . #
+        . # . . #
+        . . # # .
+        `)
     pins.digitalWritePin(DigitalPin.P1, 1)
     soundExpression.twinkle.playUntilDone()
     pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # # # . .
+        # # . . #
+        . # # # .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . # # . .
+        # . . # .
+        # . . . #
+        . # . . #
+        . . # # .
+        `)
 })
 music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+basic.showLeds(`
+    . # # . .
+    # . . # .
+    # . . . #
+    . # . . #
+    . . # # .
+    `)
 basic.forever(function () {
 	
 })
